f31ddb44cac9d161ca7c3e3bf91ac44881a63920
shm3lz <shmelv3@yandex.ru>
2021-06-15 16:31:25 +0300

    initial

---
+++ components/Layout/index.tsx
@@ -0,0 +1,19 @@
+const Layout: React.FC = ({ children }) => {
+    return (
+        <>
+            <header>
+
+            </header>
+
+            <main>
+                {children}
+            </main>
+
+            <footer>
+                
+            </footer>
+        </>
+    );
+}
+
+export default Layout;
